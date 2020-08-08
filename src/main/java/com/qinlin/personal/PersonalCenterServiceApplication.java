3be714cefdaa4223c8ef1df19ab37fdb1a6a3bad
wx909188 <806782420@qq.com>
2020-08-08 15:00:41 +0800

    first commit

---
+++ src/main/java/com/qinlin/personal/PersonalCenterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qinlin.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalCenterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalCenterServiceApplication.class, args);
+    }
+
+}
